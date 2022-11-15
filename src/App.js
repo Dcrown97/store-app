e7c4ddce90af71fe9269c62dd6e318822fe5c830
Dcrown97 <aisimiyuoluwadara@gmail.com>
2022-11-15 23:00:42 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function App() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              Learn Chakra
+              Learn Chakra gdsaihgdsa
             </Link>
           </VStack>
         </Grid>
